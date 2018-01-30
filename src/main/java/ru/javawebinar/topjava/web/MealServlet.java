18e36e7ec17d73e09d4510317101bb6f633ace30
blaec <odeskonst@yandex.ru>
2018-01-30 23:41:08 +0200

    hw01_12_0 MealList_added_to_meals.jsp

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,12 +1,18 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -17,6 +23,9 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-        response.sendRedirect("meals.jsp");
+        List<MealWithExceed> mealsWithExceed = MealsUtil.getFilteredWithExceeded(MealsUtil.getMealsList(), LocalTime.MIN, LocalTime.MAX, 2000);
+        request.setAttribute("mealList", mealsWithExceed);
+        request.getRequestDispatcher("meals.jsp").forward(request, response);
+//        response.sendRedirect("meals.jsp");
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,4 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix = "fmt" uri = "http://java.sun.com/jsp/jstl/fmt" %>
 <html>
 <head>
     <title>Meals</title>
@@ -7,4 +9,20 @@
 <h3><a href="index.html">Home</a></h3>
 <h2>Meals</h2>
 </body>
+
+<table>
+    <tr>
+        <th>DateTime</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+    <c:forEach items="${mealList}" var="meal">
+        <tr>
+            <td>${meal.dateTime}</td>
+            <td>${meal.calories}</td>
+            <td>${meal.calories}</td>
+        </tr>
+    </c:forEach>
+</table>
+
 </html>
