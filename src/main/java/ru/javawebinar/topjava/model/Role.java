e8f1aa413038fa04a4f7568407eaaf38db5bc78d
blaec <odeskonst@yandex.ru>
2018-03-01 23:24:38 +0200

    hw06_24_0 not_fixed_jdcb_test_with_set_of_roles

--- src/main/java/ru/javawebinar/topjava/model/Role.java
+++ src/main/java/ru/javawebinar/topjava/model/Role.java
@@ -1,6 +1,18 @@
 package ru.javawebinar.topjava.model;
 
 public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN
+    ROLE_USER, ROLE_ADMIN;
+
+    private Role role;
+
+    Role() {}
+
+    Role(Role role) {
+        this.role = role;
+    }
+
+
+    public Role getRole() {
+        return role;
+    }
 }
