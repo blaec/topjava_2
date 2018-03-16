a26166e5d47471532fde5fa28430712ca20f6edf
blaec <odeskonst@yandex.ru>
2018-03-16 14:41:42 +0200

    mb0905 update

--- src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.to.UserTo;
+import ru.javawebinar.topjava.util.UserUtil;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.util.List;
@@ -53,6 +56,15 @@ public class UserServiceImpl implements UserService {
     public List<User> getAll() {
         return repository.getAll();
     }
+
+    @CacheEvict(value = "users", allEntries = true)
+    @Transactional
+    @Override
+    public void update(UserTo userTo) {
+        User user = get(userTo.getId());
+        repository.save(UserUtil.updateFromTo(user, userTo));
+    }
+
 
     @CacheEvict(value = "users", allEntries = true)
     @Override
